8700883c1c68d22d1b2fb893f7f6b1e7c6929981
Erkan KILIÇ <deverkankilic@gmail.com>
2023-11-19 07:02:22 +0300

    Add project files.

---
+++ Sudoku.WinForm/Game.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Sudoku.WinForm
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
