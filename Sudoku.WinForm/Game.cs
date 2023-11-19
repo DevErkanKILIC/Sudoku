a944be935e07456866865bc10a79ed01421146f2
Erkan KILIÇ <deverkankilic@gmail.com>
2023-11-19 08:21:49 +0300

    +CreateSudokuTable function has been refactiored to take parameters.
    The components created in the +PrintSudokuTable function were written to the +CreateSudokuTableComponents function.
    
    Created +SolveSudokuTable function for Solve operation.
    
    Created +PrintSolvedSudokuTable function for print operation.
    
    +SetFirstValue function was created to make the first assignment to the table.
    
    Controller function +IsAppropriateValue added for Sudoku solving.

--- Sudoku.WinForm/Game.cs
+++ Sudoku.WinForm/Game.cs
@@ -15,6 +15,9 @@ namespace Sudoku.WinForm
         private int _txtLocationX = 0;
         private int _txtLocationY = 0;
 
+        private readonly int _firstColumnIndex;
+        private readonly int _firstRowIndex;
+
         public Game()
         {
             InitializeComponent();
@@ -22,51 +25,123 @@ namespace Sudoku.WinForm
             _sudokuTable = new int[ROW_COLUMN_SIZE, ROW_COLUMN_SIZE];
             _random = new Random();
             _txtSudokuTable = new TextBox[9, 9];
+            _firstColumnIndex = _random.Next(0, 9);
+            _firstRowIndex = _random.Next(0, 9);
         }
 
 
         private void Game_Load(object sender, EventArgs e)
         {
-            CreateSudokuTable();
-            PrintSudokuTable();
+            CreateSudokuTable(_sudokuTable);
+            CreateSudokuTableComponents();
+            PrintSolvedSudokuTable(_sudokuTable);
         }
 
-        private int[,] CreateSudokuTable()
+        private int[,] SetFirstValue(int[,] sudokuTable)
+        {
+            sudokuTable[_firstColumnIndex, _firstRowIndex] = _random.Next(1, 10);
+            return sudokuTable;
+        }
+
+        private int[,] CreateSudokuTable(int[,] sudokuTable)
+        {
+            return SetFirstValue(sudokuTable);
+        }
+
+        private bool IsAppropriateValue(int[,] sudokuTable, int column, int row, int value)
+        {
+            for (int index = 0; index < ROW_COLUMN_SIZE; index++)
+            {
+                if (sudokuTable[column, index] == value || sudokuTable[index, row] == value) return false;
+            }
+
+            int mainRow = row - row % 3;
+            int mainColumn = column - column % 3;
+
+            for (int columnIndex = mainColumn; columnIndex < mainColumn + 3; columnIndex++)
+            {
+                for (int rowIndex = mainRow; rowIndex < mainRow + 3; rowIndex++)
+                {
+                    if (sudokuTable[columnIndex, rowIndex] == value) return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool SolveSudokuTable(int[,] sudokuTable)
         {
             for (int column = 0; column < ROW_COLUMN_SIZE; column++)
             {
                 for (int row = 0; row < ROW_COLUMN_SIZE; row++)
                 {
-                    _sudokuTable[column, row] = _random.Next(0, 9);
+                    if (sudokuTable[column, row] == 0)
+                    {
+                        for (int value = 1; value <= ROW_COLUMN_SIZE; value++)
+                        {
+                            if (IsAppropriateValue(sudokuTable, column, row, value))
+                            {
+                                sudokuTable[column, row] = value;
+                                if (SolveSudokuTable(sudokuTable)) return true;
+                                sudokuTable[column, row] = 0;
+                            }
+                        }
+                        return false;
+                    }
                 }
             }
-            return _sudokuTable;
+            return true;
         }
 
-        private void PrintSudokuTable()
+        private void CreateSudokuTableComponents()
         {
             for (int column = 0; column < ROW_COLUMN_SIZE; column++)
             {
                 for (int row = 0; row < ROW_COLUMN_SIZE; row++)
                 {
                     _txtSudokuTable[column, row] = new TextBox();
-                    _txtSudokuTable[column, row].Name = $"CR-{column - row}";
-                    _txtSudokuTable[column, row].Size = _txtSize;
+                    _txtSudokuTable[column, row].Name = $"CR-{column}{row}";
                     _txtSudokuTable[column, row].Location = new Point(_txtLocationX, _txtLocationY);
-                    _txtSudokuTable[column, row].MaxLength = 1;
-                    _txtSudokuTable[column, row].KeyPress += Game_KeyPress;
-                    _txtLocationX += _txtLocationBetweenSize;
+                    _txtSudokuTable[column, row].Size = _txtSize;
                     Controls.Add(_txtSudokuTable[column, row]);
+                    _txtLocationX += _txtLocationBetweenSize;
                 }
                 _txtLocationX = 0;
                 _txtLocationY += _txtLocationBetweenSize;
             }
         }
 
+        private void PrintSudokuTable(int[,] sudokuTable)
+        {
+            for (int column = 0; column < ROW_COLUMN_SIZE; column++)
+            {
+                for (int row = 0; row < ROW_COLUMN_SIZE; row++)
+                {
+                    _txtSudokuTable[column, row].Text = sudokuTable[column, row].ToString();
+                }
+            }
+        }
+
+        private void PrintSolvedSudokuTable(int[,] sudokuTable)
+        {
+            SolveSudokuTable(sudokuTable);
+
+            for (int column = 0; column < ROW_COLUMN_SIZE; column++)
+            {
+                for (int row = 0; row < ROW_COLUMN_SIZE; row++)
+                {
+                    _txtSudokuTable[column, row].Text = sudokuTable[column, row].ToString();
+                }
+            }
+        }
+
+
+
+
         private void Game_KeyPress(object? sender, KeyPressEventArgs e)
         {
-           if(!char.IsDigit(e.KeyChar)) e.Handled = true;
-           else e.Handled = false;
+            if (!char.IsDigit(e.KeyChar)) e.Handled = true;
+            else e.Handled = false;
         }
     }
 }
